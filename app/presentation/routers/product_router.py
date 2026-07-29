ad40dc7943ce35d7932241ba9f919530aaea33d7
Ahmet Faruk Yetkin <ahmetfarukyetkin@gmail.com>
2026-07-29 09:28:34 +0300

    added product_router

--- app/presentation/routers/product_router.py
+++ app/presentation/routers/product_router.py
@@ -54,3 +54,55 @@ def get_all_products(
     service: ProductService = Depends(get_product_service),
 ):
     return service.get_all_products()
+
+def get_product_by_id(
+        product_id: int,
+        service: ProductService = Depends(get_product_service),
+):
+    try:
+        return service.get_product_by_id(product_id)
+    except ValueError as error:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail = str(error),
+        ) from error
+
+
+@router.put(
+    "/{product_id}",
+    response_model=ProductResponse,
+)
+def update_product(
+    product_id: int,
+    product_data: ProductUpdate,
+    service: ProductService = Depends(get_product_service),
+):
+    try:
+        return service.update_product(
+            product_id=product_id,
+            name=product_data.name,
+            price=product_data.price,
+            stock=product_data.stock,
+        )
+    except ValueError as error:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(error),
+        ) from error
+
+
+@router.delete(
+    "/{product_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+def delete_product(
+    product_id: int,
+    service: ProductService = Depends(get_product_service),
+):
+    try:
+        service.delete_product(product_id)
+    except ValueError as error:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(error),
+        ) from error
